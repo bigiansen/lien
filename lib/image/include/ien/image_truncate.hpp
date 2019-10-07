a71ca3d7da07632d786f6260dcdc397c2f5f7fa4
ien <ien@amsostudios.com>
2019-10-08 00:11:04 +0200

    tabs to spaces

--- lib/image/include/ien/image_truncate.hpp
+++ lib/image/include/ien/image_truncate.hpp
@@ -4,23 +4,23 @@
 
 namespace ien::img
 {
-	struct truncate_args
-	{
-		uint8_t* image_data;
-		int image_width, 
-			image_height,
-			image_channels,
-			bits_r, 
-			bits_g,
-			bits_b,
-			bits_a;
-	};
+    struct truncate_args
+    {
+        uint8_t* image_data;
+        int image_width, 
+            image_height,
+            image_channels,
+            bits_r, 
+            bits_g,
+            bits_b,
+            bits_a;
+    };
 
-	void truncate_channel_bits(const truncate_args& args);
+    void truncate_channel_bits(const truncate_args& args);
 
-	namespace _internal
-	{
-		void truncate_channel_bits_std(uint8_t* data, long size, int r, int g, int b, int a);
-		void truncate_channel_bits_sse2(uint8_t* data, long size, int r, int g, int b, int a);
-	}
+    namespace _internal
+    {
+        void truncate_channel_bits_std(uint8_t* data, long size, int r, int g, int b, int a);
+        void truncate_channel_bits_sse2(uint8_t* data, long size, int r, int g, int b, int a);
+    }
 }
--- test/image/src/image_truncate_sse.cpp
+++ test/image/src/image_truncate_sse.cpp
@@ -5,52 +5,52 @@
 
 TEST_CASE("Image SSE")
 {
-	SECTION("STD EQUAL SSE2")
-	{
-		const int sz = (1024 * 1024 * 4);
-		std::vector<uint8_t> std;
-		std::vector<uint8_t> sse;
-		std.resize(1024 * 1024 * 4);
-		sse = std;
+    SECTION("STD EQUAL SSE2")
+    {
+        const int sz = (1024 * 1024 * 4);
+        std::vector<uint8_t> std;
+        std::vector<uint8_t> sse;
+        std.resize(1024 * 1024 * 4);
+        sse = std;
 
-		ien::img::_internal::truncate_channel_bits_std(std.data(), sz, 1, 2, 3, 4);
-		ien::img::_internal::truncate_channel_bits_sse2(sse.data(), sz, 1, 2, 3, 4);
+        ien::img::_internal::truncate_channel_bits_std(std.data(), sz, 1, 2, 3, 4);
+        ien::img::_internal::truncate_channel_bits_sse2(sse.data(), sz, 1, 2, 3, 4);
 
-		for (int i = 0; i < (1024 * 1024); ++i)
-		{
-			REQUIRE(std[i] == sse[i]);
-		}
-	};
+        for (int i = 0; i < (1024 * 1024); ++i)
+        {
+            REQUIRE(std[i] == sse[i]);
+        }
+    };
 
-	BENCHMARK_ADVANCED("STANDARD")(Catch::Benchmark::Chronometer meter)
-	{
-		std::vector<uint8_t> data;
-		data.resize(1024 * 1024 * 4);
-		std::transform(data.begin(), data.end(), data.begin(), [](uint8_t b)
-		{
-			static uint8_t roll = 0;
-			return roll++;
-		});
-		meter.measure([&data]
-		{
-			ien::img::_internal::truncate_channel_bits_std(data.data(), (1024 * 1024 * 4), 1, 2, 3, 4);
-		});
-		return data;
-	};
+    BENCHMARK_ADVANCED("STANDARD")(Catch::Benchmark::Chronometer meter)
+    {
+        std::vector<uint8_t> data;
+        data.resize(1024 * 1024 * 4);
+        std::transform(data.begin(), data.end(), data.begin(), [](uint8_t b)
+        {
+            static uint8_t roll = 0;
+            return roll++;
+        });
+        meter.measure([&data]
+        {
+            ien::img::_internal::truncate_channel_bits_std(data.data(), (1024 * 1024 * 4), 1, 2, 3, 4);
+        });
+        return data;
+    };
 
-	BENCHMARK_ADVANCED("SSE2")(Catch::Benchmark::Chronometer meter)
-	{
-		std::vector<uint8_t> data;
-		data.resize(1024 * 1024 * 4);
-		std::transform(data.begin(), data.end(), data.begin(), [](uint8_t b)
-		{
-			static uint8_t roll = 0;
-			return roll++;
-		});
-		meter.measure([&data]
-		{
-			ien::img::_internal::truncate_channel_bits_sse2(data.data(), (1024 * 1024 * 4), 1, 2, 3, 4);
-		});
-		return data;
-	};
+    BENCHMARK_ADVANCED("SSE2")(Catch::Benchmark::Chronometer meter)
+    {
+        std::vector<uint8_t> data;
+        data.resize(1024 * 1024 * 4);
+        std::transform(data.begin(), data.end(), data.begin(), [](uint8_t b)
+        {
+            static uint8_t roll = 0;
+            return roll++;
+        });
+        meter.measure([&data]
+        {
+            ien::img::_internal::truncate_channel_bits_sse2(data.data(), (1024 * 1024 * 4), 1, 2, 3, 4);
+        });
+        return data;
+    };
 };
