11cb15236f670487227eff1f6ef320482b0ce2e1
ien <ien@amsostudios.com>
2019-10-08 01:20:11 +0200

    max_channel_rgba with SSE2 and SSSE3 implementations + tests

---
+++ lib/image/include/ien/image_ops.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cinttypes>
+#include <vector>
+
+namespace ien::img
+{
+    //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // TRUNCATE
+    //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    struct truncate_args
+    {
+        uint8_t* image_data;
+        int image_width, 
+            image_height,
+            image_channels,
+            bits_r, 
+            bits_g,
+            bits_b,
+            bits_a;
+    };
+    void truncate_channel_bits(const truncate_args& args);
+    namespace _internal
+    {
+        void truncate_channel_bits_std(uint8_t* data, size_t size, int r, int g, int b, int a);
+        void truncate_channel_bits_sse2(uint8_t* data, size_t size, int r, int g, int b, int a);
+    }
+
+    //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    // MAX CHANNEL RGBA
+    //+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+    std::vector<uint8_t> max_channel_rgba(const uint8_t* data, size_t len);
+    namespace _internal
+    {
+        std::vector<uint8_t> max_channel_rgba_std(const uint8_t* data, size_t len);
+		std::vector<uint8_t> max_channel_rgba_sse2(const uint8_t* data, size_t len);
+        std::vector<uint8_t> max_channel_rgba_ssse3(const uint8_t* data, size_t len);
+    }
+}
--- test/image/src/image_truncate_sse.cpp
+++ test/image/src/image_truncate_sse.cpp
@@ -1,9 +1,9 @@
 #define CATCH_CONFIG_ENABLE_BENCHMARKING
 #include <catch2/catch.hpp>
 
-#include <ien/image_truncate.hpp>
+#include <ien/image_ops.hpp>
 
-TEST_CASE("Image SSE")
+TEST_CASE("Image truncate")
 {
     SECTION("STD EQUAL SSE2")
     {
@@ -22,6 +22,7 @@ TEST_CASE("Image SSE")
         }
     };
 
+	#ifdef NDEBUG
     BENCHMARK_ADVANCED("STANDARD")(Catch::Benchmark::Chronometer meter)
     {
         std::vector<uint8_t> data;
@@ -53,4 +54,89 @@ TEST_CASE("Image SSE")
         });
         return data;
     };
+	#endif
+};
+
+TEST_CASE("Image max")
+{
+	SECTION("STD EQUAL SSSE3 EQUAL SSE2")
+	{
+		const int sz = (1024 * 1024 * 4);
+		std::vector<uint8_t> std, sse2, ssse3;
+		std.resize(1024 * 1024 * 4);		
+		std::transform(std.begin(), std.end(), std.begin(), [](uint8_t)
+		{
+			return static_cast<uint8_t>(rand());
+		});
+		sse2 = std;
+		ssse3 = std;
+
+		std::vector<uint8_t> max_data_ssse3 =
+			ien::img::_internal::max_channel_rgba_ssse3(sse2.data(), sse2.size());
+
+		std::vector<uint8_t> max_data_std =
+			ien::img::_internal::max_channel_rgba_std(std.data(), std.size());
+
+		std::vector<uint8_t> max_data_sse2 =
+			ien::img::_internal::max_channel_rgba_sse2(std.data(), std.size());
+
+		REQUIRE(max_data_sse2.size() == max_data_ssse3.size());
+		REQUIRE(max_data_sse2.size() == max_data_std.size());
+		for (size_t i = 0; i < max_data_std.size(); ++i)
+		{
+			INFO("Index: " + std::to_string(i));
+			REQUIRE(max_data_std[i] == max_data_sse2[i]);
+			REQUIRE(max_data_std[i] == max_data_ssse3[i]);
+		}
+	};
+
+	#ifdef NDEBUG
+	BENCHMARK_ADVANCED("STANDARD")(Catch::Benchmark::Chronometer meter)
+	{
+		std::vector<uint8_t> data;
+		data.resize(1024 * 1024 * 4);
+		std::transform(data.begin(), data.end(), data.begin(), [](uint8_t b)
+		{
+			static uint8_t roll = 0;
+			return roll++;
+		});
+		meter.measure([&data]
+		{
+			return ien::img::_internal::max_channel_rgba_std(data.data(), data.size());
+		});
+		return data;
+	};
+
+	BENCHMARK_ADVANCED("SSSE3")(Catch::Benchmark::Chronometer meter)
+	{
+		std::vector<uint8_t> data;
+		data.resize(1024 * 1024 * 4);
+		std::transform(data.begin(), data.end(), data.begin(), [](uint8_t b)
+		{
+			static uint8_t roll = 0;
+			return roll++;
+		});
+		meter.measure([&data]
+		{
+			return ien::img::_internal::max_channel_rgba_ssse3(data.data(), data.size());
+		});
+		return data;
+	};
+
+	BENCHMARK_ADVANCED("SSE2")(Catch::Benchmark::Chronometer meter)
+	{
+		std::vector<uint8_t> data;
+		data.resize(1024 * 1024 * 4);
+		std::transform(data.begin(), data.end(), data.begin(), [](uint8_t b)
+		{
+			static uint8_t roll = 0;
+			return roll++;
+		});
+		meter.measure([&data]
+		{
+			return ien::img::_internal::max_channel_rgba_ssse3(data.data(), data.size());
+		});
+		return data;
+	};
+	#endif
 };
