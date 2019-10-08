a42d27ee52dbf872cb8a28ff934bb7cf37fc015e
ien <ien@amsostudios.com>
2019-10-08 23:24:22 +0200

    set default stb alignment to 32 bits (AVX support)

--- lib/image/third-party/stb/src/stb_image.impl.cpp
+++ lib/image/third-party/stb/src/stb_image.impl.cpp
@@ -3,15 +3,21 @@
 #define STBI_NO_PIC
 #define STBI_NO_PNM
 
+// Set default alignment of stbi malloc to 32 bits (AVX)
+#ifndef LIEN_STB_ALLIGN_MALLOC
+    #define LIEN_STB_ALLIGN_MALLOC 32
+#endif
+
 #include <ien/platform.hpp>
+
 // Align to 16byte on SSE2 compatible platforms
 #if defined(LIEN_ARCH_X86_64) || defined(LIEN_ARCH_X86)
     #if defined(LIEN_OS_WIN64) || defined(LIEN_OS_WIN32) // thanks microsoft
-        #define STBI_MALLOC(sz) _aligned_malloc(sz, 16)
+        #define STBI_MALLOC(sz) _aligned_malloc(sz, LIEN_STBI_ALLIGN_MALLOC)
         #define STBI_FREE(h) _aligned_free(h)
-        #define STBI_REALLOC(h, sz) _aligned_realloc(h, sz, 16)
+        #define STBI_REALLOC(h, sz) _aligned_realloc(h, sz, LIEN_STBI_ALLIGN_MALLOC)
     #else
-        #define STBI_MALLOC(sz) std::aligned_alloc(16, sz)
+        #define STBI_MALLOC(sz) std::aligned_alloc(LIEN_STBI_ALLIGN_MALLOC, sz)
         #define STBI_FREE(h) std::free(h)
         #define STBI_REALLOC(h, sz) std::realloc(h, sz)
     #endif
