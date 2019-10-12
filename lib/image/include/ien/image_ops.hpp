505469d1a4f44ae20110496d4ec21b2c521d0b88
ien <ien@amsostudios.com>
2019-10-12 02:43:41 +0200

    image channels average value algorithm

--- lib/image/include/ien/image_ops.hpp
+++ lib/image/include/ien/image_ops.hpp
@@ -1,6 +1,9 @@
 #include <ien/image.hpp>
 #include <ien/platform.hpp>
 
+#include <cinttypes>
+#include <vector>
+
 namespace ien::img
 {
     void truncate_channel_data(image* img, int bits_r, int bits_g, int bits_b, int bits_a);
@@ -9,9 +12,9 @@ namespace ien::img
         struct truncate_channel_args
         {
             size_t len;
-            uint8_t* ch_r; 
-            uint8_t* ch_g; 
-            uint8_t* ch_b; 
+            uint8_t* ch_r;
+            uint8_t* ch_g;
+            uint8_t* ch_b;
             uint8_t* ch_a;
             int bits_r;
             int bits_g; 
@@ -24,6 +27,27 @@ namespace ien::img
         #if defined(LIEN_ARCH_X86) || defined(LIEN_ARCH_X86_64)
         void truncate_channel_data_sse2(const truncate_channel_args& args);
         void truncate_channel_data_avx2(const truncate_channel_args& args);
+
+        #endif
+    }
+
+    std::vector<uint8_t> channel_average(const image* img);
+    namespace _internal
+    {
+        struct channel_average_args
+        {
+            size_t len;
+            const uint8_t* ch_r;
+            const uint8_t* ch_g;
+            const uint8_t* ch_b;
+            const uint8_t* ch_a;
+        };
+
+        std::vector<uint8_t> channel_average_std(const channel_average_args& args);
+
+        #if defined(LIEN_ARCH_X86) || defined(LIEN_ARCH_X86_64)
+        std::vector<uint8_t> channel_average_sse2(const channel_average_args& args);
+        std::vector<uint8_t> channel_average_avx2(const channel_average_args& args);
 
         #endif
     }
--- lib/image/src/image_ops.cpp
+++ lib/image/src/image_ops.cpp
@@ -7,6 +7,7 @@ namespace ien::img
     void truncate_channel_data(image_unpacked_data* img, int bits_r, int bits_g, int bits_b, int bits_a)
     {
         typedef void(*func_ptr_t)(const _internal::truncate_channel_args& args);
+
         #if defined(LIEN_ARCH_X86) || defined(LIEN_ARCH_X86_64)
         static func_ptr_t func = 
             platform::x86::get_feature(platform::x86::feature::AVX2)
@@ -72,9 +73,9 @@ namespace ien::img
 
         void truncate_channel_data_sse2(const truncate_channel_args& args)
         {
-            static const int SSE2_VECSZ = 16;
+            static const int SSE2_STRIDE = 16;
             const size_t img_sz = args.len;
-            if(img_sz < SSE2_VECSZ)
+            if(img_sz < SSE2_STRIDE)
             {
                 truncate_channel_data_std(args);
                 return;
@@ -90,7 +91,7 @@ namespace ien::img
             const __m128i vmask_b = _mm_set1_epi32(trunc_and_table[args.bits_b]);
             const __m128i vmask_a = _mm_set1_epi32(trunc_and_table[args.bits_a]);
 
-            for(size_t i = 0; i < img_sz; i += 16)
+            for(size_t i = 0; i < img_sz; i += SSE2_STRIDE)
             {
                 __m128i seg_r = _mm_load_si128(reinterpret_cast<__m128i*>(r + i));
                 __m128i seg_g = _mm_load_si128(reinterpret_cast<__m128i*>(g + i));
@@ -108,7 +109,7 @@ namespace ien::img
                 _mm_store_si128(reinterpret_cast<__m128i*>(a + i), seg_a);
             }
 
-            size_t remainder_idx = img_sz - (img_sz % 16);
+            size_t remainder_idx = img_sz - (img_sz % SSE2_STRIDE);
             for(size_t i = remainder_idx; i < img_sz; ++i)
             {
                 r[i] &= trunc_and_table[args.bits_r];
@@ -164,6 +165,147 @@ namespace ien::img
                 b[i] &= trunc_and_table[args.bits_b];
                 a[i] &= trunc_and_table[args.bits_a];
             }
+        }
+
+        #endif
+    }
+
+    std::vector<uint8_t> channel_average(const image* img)
+    {
+        typedef std::vector<uint8_t>(*func_ptr_t)(const _internal::channel_average_args&);
+
+        #if defined(LIEN_ARCH_X86) || defined(LIEN_ARCH_X86_64)
+        static func_ptr_t func = 
+            platform::x86::get_feature(platform::x86::feature::AVX2)
+            ? &_internal::channel_average_avx2
+            : platform::x86::get_feature(platform::x86::feature::SSE2)
+            ? &_internal::channel_average_sse2
+            : &_internal::channel_average_std;
+
+        #else
+        static func_ptr_t func = &_internal::chanchannel_average_std;
+
+        #endif
+
+        _internal::channel_average_args args = {
+            img->pixel_count(),
+            img->cdata()->cdata_r(), 
+            img->cdata()->cdata_g(), 
+            img->cdata()->cdata_b(), 
+            img->cdata()->cdata_a()
+        };
+
+        func(args);
+    }
+
+    namespace _internal
+    {
+        std::vector<uint8_t> channel_average_std(const channel_average_args& args)
+        {
+            const size_t img_sz = args.len;
+            
+            std::vector<uint8_t> result;
+            result.resize(args.len);
+
+            const uint8_t* r = args.ch_r;
+            const uint8_t* g = args.ch_g;
+            const uint8_t* b = args.ch_b;
+            const uint8_t* a = args.ch_a;
+
+            for(size_t i = 0; i < img_sz; ++i)
+            {
+                uint16_t sum = static_cast<uint16_t>(r[i]) + g[i] + b[i] + a[i];
+                result[i] = static_cast<uint8_t>(sum / 4);
+            }
+            return result;
+        }
+
+        #if defined(LIEN_ARCH_X86) || defined(LIEN_ARCH_X86_64)
+
+        std::vector<uint8_t> channel_average_sse2(const channel_average_args& args)
+        {
+            static const size_t SSE2_STRIDE = 16;
+            const size_t img_sz = args.len;
+
+            if(img_sz < SSE2_STRIDE)
+            {
+                channel_average_std(args);
+                return;
+            }
+            
+            std::vector<uint8_t> result;
+            result.resize(args.len);
+            
+            const uint8_t* r = args.ch_r;
+            const uint8_t* g = args.ch_g;
+            const uint8_t* b = args.ch_b;
+            const uint8_t* a = args.ch_a;
+
+            for(size_t i = 0; i < img_sz; i += SSE2_STRIDE)
+            {
+                __m128i vseg_r = _mm_load_si128(reinterpret_cast<const __m128i*>(r + i));
+                __m128i vseg_g = _mm_load_si128(reinterpret_cast<const __m128i*>(g + i));
+                __m128i vseg_b = _mm_load_si128(reinterpret_cast<const __m128i*>(b + i));
+                __m128i vseg_a = _mm_load_si128(reinterpret_cast<const __m128i*>(a + i));
+
+                __m128i vavg_rg = _mm_avg_epu8(vseg_r, vseg_g);
+                __m128i vavg_ba = _mm_avg_epu8(vseg_r, vseg_g);
+
+                __m128i vagv_rgba = _mm_avg_epu8(vavg_rg, vavg_ba);
+
+                _mm_store_si128(reinterpret_cast<__m128i*>(result.data() + i), vagv_rgba);
+            }
+
+            size_t remainder_idx = img_sz - (img_sz % SSE2_STRIDE);
+            for(size_t i = remainder_idx; i < img_sz; ++i)
+            {
+                uint16_t sum = static_cast<uint16_t>(r[i]) + g[i] + b[i] + a[i];
+                result[i] = static_cast<uint8_t>(sum / 4);
+            }
+            return result;
+        }
+
+        std::vector<uint8_t> channel_average_avx2(const channel_average_args& args)
+        {
+            static const size_t AVX2_STRIDE = 32;
+            const size_t img_sz = args.len;
+
+            if(img_sz < AVX2_STRIDE)
+            {
+                channel_average_std(args);
+                return;
+            }
+            
+            std::vector<uint8_t> result;
+            result.resize(args.len);
+            
+            const uint8_t* r = args.ch_r;
+            const uint8_t* g = args.ch_g;
+            const uint8_t* b = args.ch_b;
+            const uint8_t* a = args.ch_a;
+
+            for(size_t i = 0; i < img_sz; i += AVX2_STRIDE)
+            {
+                __m256i vseg_r = _mm256_load_si256(reinterpret_cast<const __m256i*>(r + i));
+                __m256i vseg_g = _mm256_load_si256(reinterpret_cast<const __m256i*>(g + i));
+                __m256i vseg_b = _mm256_load_si256(reinterpret_cast<const __m256i*>(b + i));
+                __m256i vseg_a = _mm256_load_si256(reinterpret_cast<const __m256i*>(a + i));
+
+                __m256i vavg_rg = _mm256_avg_epu8(vseg_r, vseg_g);
+                __m256i vavg_ba = _mm256_avg_epu8(vseg_r, vseg_g);
+
+                __m256i vagv_rgba = _mm256_avg_epu8(vavg_rg, vavg_ba);
+
+                _mm256_store_si256(reinterpret_cast<__m256i*>(result.data() + i), vagv_rgba);
+            }
+
+            size_t remainder_idx = img_sz - (img_sz % AVX2_STRIDE);
+            for(size_t i = remainder_idx; i < img_sz; ++i)
+            {
+                uint16_t sum = static_cast<uint16_t>(r[i]) + g[i] + b[i] + a[i];
+                result[i] = static_cast<uint8_t>(sum / 4);
+            }
+            return result;
         }
 
         #endif
