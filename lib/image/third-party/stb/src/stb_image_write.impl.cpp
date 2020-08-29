f554afaedbad0354018223ae8787ab6b858d9a84
ien <ien@amsostudios.com>
2020-08-30 00:05:27 +0200

    remove zlib compression for stb (buggy on wasm)

--- lib/image/third-party/stb/src/stb_image_write.impl.cpp
+++ lib/image/third-party/stb/src/stb_image_write.impl.cpp
@@ -1,19 +1,3 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #define STBI_MSC_SECURE_CRT
-
-#include <zlib.h>
-#include <cstdlib>
-
-unsigned char* lien_zlib_compress(unsigned char *data, int data_len, int *out_len, int quality)
-{
-    uLongf len;
-    unsigned char* dest = reinterpret_cast<unsigned char*>(
-        std::malloc(compressBound(data_len))
-    );
-    compress2(dest, &len, data, data_len, quality);
-    *out_len = static_cast<int>(len);
-    return dest;
-}
-
-#define STBIW_ZLIB_COMPRESS lien_zlib_compress
 #include "stb_image_write.h"
